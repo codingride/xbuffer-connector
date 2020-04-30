6906051b46b2019c568d1b003ea9bd2fcf38b7af
Abdullah Alghamdi <abdullah@manzilak.com>
2020-04-30 09:29:04 +0300

    Update get.js

--- example/get.js
+++ example/get.js
@@ -5,8 +5,10 @@
 var getRequest = {
   path: '',
   method: 'get',
-  type: 'data',
-  request: 'collection_name' // Collection name in the database
+  data: {
+    type: 'data',
+    request: 'collection_name' // Collection name in the database
+  }
 }
 
 /**
@@ -15,9 +17,11 @@ var getRequest = {
 var getRequest = {
   path: '',
   method: 'get',
-  type: 'data',
-  request: 'collection_name', // Collection name in the database
-  record: 'key_name' // Will get all documents in the collection that has this key
+  data: {
+    type: 'data',
+    request: 'collection_name', // Collection name in the database
+    record: 'key_name' // Will get all documents in the collection that has this key
+  }
 }
 
 /**
@@ -26,9 +30,11 @@ var getRequest = {
 var getRequest = {
   path: '',
   method: 'get',
-  type: 'data',
-  request: 'collection_name', // Collection name in the database
-  id: 'id1...,id2....' // You can get a single document or multble documents by passing an ID or more seperated by comma 
+  data: {
+    type: 'data',
+    request: 'collection_name', // Collection name in the database
+    id: 'id1...,id2....' // You can get a single document or multble documents by passing an ID or more seperated by comma 
+  }
 }
 
 /**
@@ -37,9 +43,11 @@ var getRequest = {
 var getRequest = {
   path: '',
   method: 'get',
-  type: 'data',
-  request: 'collection_name', // Collection name in the database
-  getby: 'key_one:value,key_two:value' // Will get all documents that has the key with the value assigned. You can pass more key value pares seprated by comma
+  data: {
+    type: 'data',
+    request: 'collection_name', // Collection name in the database
+    getby: 'key_one:value,key_two:value' // Will get all documents that has the key with the value assigned. You can pass more key value pares seprated by comma
+  }
 }
 
 /**
@@ -49,11 +57,13 @@ var getRequest = {
 var getRequest = {
   path: '',
   method: 'get',
-  type: 'data',
-  request: 'collection_name', // Collection name in the database
-  offset: 0, // This is to set where to begin requesting documents. It begins with 0 then 10 if you want to show only 10 per request.
-  max: 100// This can be up to 100 document per request. This is going to be changed to "limit" instead of "max" in the next version
-  // Each data get request will give out the total size documents that matches your query so you can easily build your pagination. The response key will be "count"
+  data: {
+    type: 'data',
+    request: 'collection_name', // Collection name in the database
+    offset: 0, // This is to set where to begin requesting documents. It begins with 0 then 10 if you want to show only 10 per request.
+    max: 100// This can be up to 100 document per request. This is going to be changed to "limit" instead of "max" in the next version
+    // Each data get request will give out the total size documents that matches your query so you can easily build your pagination. The response key will be "count"
+  }
 }
 
 /**
