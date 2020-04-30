0c4d70fa74a23d84ee96b92d2af0ea88ae6d1b4b
Abdullah Alghamdi <abdullah@manzilak.com>
2020-04-30 07:59:22 +0300

    Update put.js

--- example/put.js
+++ example/put.js
@@ -12,11 +12,14 @@
 var getRequest = {
   path: '',
   method: 'put',
-  type: 'data',
-  id: 'id01....', // This must to be provided to know what document needs to be changed
-  request: 'collection_name', // Collection name in the database
-  record: 'key_name', // Add the targted destnation where you want to add the new data.
-  data: "new data" // It could be another object like { "new_key":"new_val" }
+  headers: true, // Set true if write to database is restricted
+  data: {
+    type: 'data',
+    id: 'id01....', // This must to be provided to know what document needs to be changed
+    request: 'collection_name', // Collection name in the database
+    record: 'key_name', // Add the targted destnation where you want to add the new data.
+    data: "new data" // It could be another object like { "new_key":"new_val" }
+  }
 }
 
 /**
